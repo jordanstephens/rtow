385d4457a94193e255415617a80e9847ee9b8dda
Jordan Stephens <jordan@stephens.io>
2020-07-21 14:08:37 -0500

    8 diffuse material

--- src/main.cc
+++ src/main.cc
@@ -7,11 +7,17 @@
 #include "rtweekend.h"
 #include "sphere.h"
 
-color ray_color(const ray& r, const hittable& world) {
-  hit_record rec;
-  if (world.hit(r, 0, infinity, rec)) {
-    return 0.5 * (rec.normal + color(1, 1, 1));
+color ray_color(const ray& r, const hittable& world, int depth) {
+  if (depth <= 0) {
+    return color(0, 0, 0);
   }
+
+  hit_record rec;
+  if (world.hit(r, 0.001, infinity, rec)) {
+    point3 target = rec.p + rec.normal + random_in_hemisphere(rec.normal);
+    return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
+  }
+
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -20,15 +26,17 @@ color ray_color(const ray& r, const hittable& world) {
 int main(int argc, char const* argv[]) {
   if (argc < 3) {
     std::cerr << "Usage: " << argv[0] << " <width>"
-              << " <height>" << std::endl;
+              << " <height>"
+              << " <samples>" << std::endl;
     return 1;
   }
 
   // image dimensions
   const int width = atoi(argv[1]);
   const int height = atoi(argv[2]);
+  const int samples = atoi(argv[3]);
   const auto aspect_ratio = double(width) / double(height);
-  const int samples_per_pixel = 100;
+  const int max_depth = 50;
 
   // world
   hittable_list world;
@@ -44,13 +52,13 @@ int main(int argc, char const* argv[]) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < width; ++i) {
       color pixel_color(0, 0, 0);
-      for (int s = 0; s < samples_per_pixel; ++s) {
+      for (int s = 0; s < samples; ++s) {
         auto u = (i + random_double()) / (width - 1);
         auto v = (j + random_double()) / (height - 1);
         ray r = cam.get_ray(u, v);
-        pixel_color += ray_color(r, world);
+        pixel_color += ray_color(r, world, max_depth);
       }
-      write_color(std::cout, pixel_color, samples_per_pixel);
+      write_color(std::cout, pixel_color, samples);
     }
   }
 
